03d3c0fce29fa97f949e0ac0a0067752059c5feb
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-07-01 18:07:08 +0000

    Update terraform.tf

--- harness-resources/terraform.tf
+++ harness-resources/terraform.tf
@@ -2,7 +2,8 @@ terraform {
     required_providers {  
         harness = {  
             source = "harness/harness"  
-            version = ">=0.37.5"  
+ #           version = ">=0.37.5"  
+            version = "0.24.2"  
         }  
         azurerm = {
             source  = "hashicorp/azurerm"
