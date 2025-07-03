0fd06eef188a2a64e359e9eb4a15fe6076237b16
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-07-03 14:25:16 +0000

    Update main.tf

--- harness-resources/main.tf
+++ harness-resources/main.tf
@@ -1,4 +1,5 @@
 locals {
+  envs = var.envs
   resourcegroup_names = [for rg in split("\n", file("./repo-list.txt")) : rg if rg != ""]
   workspaces = flatten([
     for resourcegroup_name in local.resourcegroup_names : [
