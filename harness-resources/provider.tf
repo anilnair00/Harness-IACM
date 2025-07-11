75e291957a30bde1d85a268fe1333abb15191a5d
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-07-11 07:34:02 +0000

    Delete harness-resources directory

--- harness-resources/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "harness" {
-  endpoint         = var.TF_VAR_HARNESS_ENDPOINT
-  account_id       = var.TF_VAR_HARNESS_ACCOUNT_ID
-  platform_api_key = var.TF_VAR_HARNESS_PLATFORM_API_KEY
-}
-provider "azurerm" {
-  features {}
-  use_oidc        = true
-}
