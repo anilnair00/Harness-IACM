f3db6b5a53daab565e6a0422a81aec4b1fa53b2f
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-07-14 10:10:39 +0000

    Update main.tf

--- project-1/main.tf
+++ project-1/main.tf
@@ -113,7 +113,7 @@ locals {
                         operator: Equals
                         value: develop
                     headerConditions: []
-                   repoName: Harness-IACM
+                   repoName: ${var.repository_name}
                     actions:
                       - Open
                       - Edit
